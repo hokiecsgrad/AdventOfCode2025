0e2003c3842b5335c6176d12f4b636c2a21753c2
Ryan Hagan <ryan@ryanhagan.net>
2025-12-07 20:58:34 +0000

    Solved Day 7

--- Day07/src/Solver.cs
+++ Day07/src/Solver.cs
@@ -12,13 +12,13 @@ public class Solver
         (int row, int col) startPos = FindCharPos(grid, 'S');
         laserPositions.Push(startPos);
 
-        DrawGrid(grid);
+        // DrawGrid(grid);
 
         int numBeamSplits = 0;
         while (laserPositions.Count > 0)
         {
             (int row, int col) currPos = laserPositions.Pop();
-            if (currPos.row == grid.GetLength(0) - 1 || currPos.col < 0 || currPos.col > grid.GetLength(1) - 1)
+            if (currPos.row == grid.GetLength(0) - 1)
             {
                 continue;
             }
@@ -41,7 +41,7 @@ public class Solver
 
         }
 
-        DrawGrid(grid);
+        // DrawGrid(grid);
         return numBeamSplits.ToString();
     }
 
@@ -49,13 +49,42 @@ public class Solver
     {
         char[,] grid = CreateGrid(data);
 
-        Stack<(int row, int col)> laserPositions = new();
+        // create a cache for memoization
+        long[,] gridCache = new long[grid.GetLength(0), grid.GetLength(1)];
+        for (int row = 0; row < grid.GetLength(0); row++)
+            for (int col = 0; col < grid.GetLength(1); col++)
+                gridCache[row, col] = 0;
+
+        // find the start position
         (int row, int col) startPos = FindCharPos(grid, 'S');
-        laserPositions.Push(startPos);
 
-        int numTimelines = 0;
+        // recursively test out all the paths
+        long uniquePaths = FindPaths(grid, (startPos.row + 1, startPos.col), gridCache);
 
-        return numTimelines.ToString();
+        return uniquePaths.ToString();
+    }
+
+    private long FindPaths(char[,] grid, (int row, int col) currPos, long[,] gridCache)
+    {
+        while (currPos.row <= grid.GetLength(0) - 1 && grid[currPos.row, currPos.col] == '.')
+            currPos.row++;
+        if (currPos.row > grid.GetLength(0) - 1) return 1;
+
+        long numPaths = 0;
+        if (grid[currPos.row, currPos.col] == '^')
+        {
+            if (gridCache[currPos.row, currPos.col] != 0)
+                return gridCache[currPos.row, currPos.col];
+            else
+            {
+                numPaths = FindPaths(grid, (currPos.row, currPos.col - 1), gridCache);
+                numPaths += FindPaths(grid, (currPos.row, currPos.col + 1), gridCache);
+            }
+        }
+
+        gridCache[currPos.row, currPos.col] = numPaths;
+
+        return numPaths;
     }
 
     private char[,] CreateGrid(string[] data)
